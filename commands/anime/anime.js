7502b3bc4baea95a265347ca0f3fba24e0257490
Tokioshy <grizzlizyping@gmail.com>
2023-06-05 09:03:11 -0700

    some change

--- commands/anime/anime.js
+++ commands/anime/anime.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const get = require('request-promise-native');
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { get } = require('request-promise-native');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,12 +10,13 @@ module.exports = {
         .setDescription('Judul anime yang ingin dicari')
         .setRequired(true)
       )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .setDMPermission(false),
   async execute(interaction) {
     let title = interaction.options.getString('judul');
 
     let option = {
-      url: `https://kitsu.io/api/edge/anime?filter[text]=${encodeURIComponent(title)}`,
+      url: `https://kitsu.io/api/edge/anime?filter[text]=${title}&page[limit]=3`,
       method: 'GET',
       headers: {
         'Content-Type' : 'application/vnd.api+json',
@@ -32,24 +33,26 @@ module.exports = {
       ]
     }).then(() => {
       get(option).then((mat) => {
+        let sts = mat.data[0].attributes.status;
+        let up = sts.charAt(0).toUpperCase() + sts.slice(1);
+
         interaction.editReply({
           embeds: [
             new EmbedBuilder()
             .setColor('Navy')
             .setTitle(mat.data[0].attributes.titles.en_jp)
             .setURL(`https://kitsu.io/anime/${mat.data[0].id}`)
-            .setThumbnail(mat.data[0].attributes.posterImage.tiny)
+            .setThumbnail(mat.data[0].attributes.posterImage.original)
             .setDescription(mat.data[0].attributes.synopsis)
             .addFields(
-              { name: 'Anime', value: `${mat.data[0].subtype}`, inline: true },
+              { name: 'Anime', value: `${mat.data[0].attributes.subtype}`, inline: true },
               { name: 'Episode', value: `${mat.data[0].attributes.episodeCount ? mat.data[0].attributes.episodeCount : 'N/A'}`, inline: true },
               { name: 'Duration', value: `${mat.data[0].attributes.episodeLength ? mat.data[0].attributes.episodeLength : 'N/A'}`, inline: true },
-              { name:' Status', value: `${mat.data[0].status}`, inline: true },
-              { name: 'Start Date', value: `${mat.data[0].startDate}`, inline: true },
-              { name: 'End Date', value: `${mat.data[0].endDate ? mat.data[0].endDate : 'N/A'}`, inline: true },
-              { name: 'Popularity Rank', value: `#${mat.data[0].popularityRank}` },
+              { name:' Status', value: `${up}`, inline: true },
+              { name: 'Start Date', value: `${mat.data[0].attributes.startDate}`, inline: true },
+              { name: 'End Date', value: `${mat.data[0].attributes.endDate ? mat.data[0].attributes.endDate : 'N/A'}`, inline: true },
             )
-            .setFooter({ name: `${interaction.client.user.username}`, iconURL: interaction.client.user.displayAvatarURL({ extension: 'png' }) })
+            .setFooter({ text: `${interaction.client.user.username}`, iconURL: interaction.client.user.displayAvatarURL({ extension: 'png' }) })
             .setTimestamp()
           ]
         });
