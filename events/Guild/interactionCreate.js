650bc4ce4322cfa6b2fbdecb5e894459186b9336
Tokioshy <grizzlizyping@gmail.com>
2023-06-07 11:54:18 +0700

    verify

--- events/Guild/interactionCreate.js
+++ events/Guild/interactionCreate.js
@@ -31,5 +31,40 @@ module.exports = {
         console.log(error);
       };
     };
+
+    if(interaction.isButton()) {
+      if(interaction.customId == 'verify') {
+        if(interaction.member.roles.cache.has('1089027827975864343')) {
+          return interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+              .setColor('Red')
+              .setDescription('⛔ | You already verified!')
+            ],
+            ephemeral: true
+          });
+        };
+
+        await interaction.member.roles.add('1089027827975864343').then(() => {
+          interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+              .setColor('Green')
+              .setDescription('✅ | Successfully verify!')
+            ],
+            ephemeral: true
+          });
+        }).catch((error) => {
+          interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+              .setColor9('Red')
+              .setDescription('⛔ | Fail verify, please report to <@1010474132753883207>!')
+            ],
+            ephemeral: true
+          });
+        });
+      };
+    };
   },
 };
