d6adbaedbd99739121b0a9c0c1b48907067a5ca2
Tokioshy <grizzlizyping@gmail.com>
2023-06-07 11:48:46 +0700

    add verify

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -1,4 +1,4 @@
-const { Events, ActivityType } = require('discord.js');
+const { Events, ActivityType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
   name: Events.ClientReady,
@@ -6,5 +6,25 @@ module.exports = {
   async execute(client) {
     console.log(`Ready and logged as ${client.user.tag}!`);
     client.user.setActivity('Competitive', { type: ActivityType.Playing });
+
+    client.channels.cache.get('1093321530378813560').send({
+      embeds: [
+        new EmbedBuilder()
+        .setColor('Navy')
+        .setTitle('Verification Required')
+        .setDescription('> If you want to unlock additional channels on Hapori, click the button below and the bot will automatically give you the <@&1089027827975864343> role.')
+        .setFooter({ text: '・Hapori Staff Team' })
+      ],
+      components: [
+        new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+          .setCustomId('verify')
+          .setLabel('Verify')
+          .setEmoji('✅')
+          .setStyle(ButtonStyle.Success)
+        )
+      ],
+    });
   },
 };
